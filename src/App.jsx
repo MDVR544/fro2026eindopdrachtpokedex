4e575608ff5bd26322d8ac367313c350c0c585ff
MDVR544 <devriescool@gmail.com>
2026-06-25 21:36:23 +0200

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import './App.css'
 import axios from 'axios';
 import Button from "./Components/normal button/normal button.jsx";
+import {Routes, Route} from "react-router-dom";
 import Pokeball from "./Components/pokéball/pokéball.jsx";
-import DropdownMenu from "./Components/dropdown menu/dropdown menu.jsx";
+import Account from "./pages/account/account.jsx";
+import BattleAdvice from "./pages/battleAdvice/battleAdvice.jsx";
+import Favorites from "./pages/favorites/favorites.jsx";
+import MyTeams from "./pages/myTeams/myTeams.jsx";
+import Pokedex from "./pages/pokédex/pokedex.jsx";
+import NotFound from "./pages/notFound/notFound.jsx";
+import NavBar from "./Components/navbar/navBar.jsx";
 
 function App() {
     // import.meta.env.VITE_API_KEY
 
   return (
     <>
-        <Button />
-        <Pokeball />
-        <DropdownMenu />
-
+    <NavBar />
+        <Routes>
+            <Route path="account" element={<Account/>} />
+            <Route path="battleadvice" element={<BattleAdvice />} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="myteams" element={<MyTeams />} />
+            <Route path="/" element={<Pokedex />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
     </>
   )
 }
---
+++ src/pages/pokédex/pokedex.jsx
@@ -0,0 +1,13 @@
+import './pokedex.css' ;
+
+
+function Pokedex(){
+
+    return (
+        <>
+            <h1>Pokédex page</h1>
+        </>
+    );
+}
+
+export default Pokedex
