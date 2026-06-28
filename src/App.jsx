ac3ff5c7f18f5e60ffa54a091ffb3b92a5dbbf40
MDVR544 <devriescool@gmail.com>
2026-06-28 13:20:08 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
 import axios from 'axios';
-import Button from "./Components/normal button/normal button.jsx";
 import {Routes, Route} from "react-router-dom";
-import Pokeball from "./Components/pokéball/pokéball.jsx";
 import Account from "./pages/account/account.jsx";
 import BattleAdvice from "./pages/battleAdvice/battleAdvice.jsx";
 import Favorites from "./pages/favorites/favorites.jsx";
@@ -10,6 +8,7 @@ import MyTeams from "./pages/myTeams/myTeams.jsx";
 import Pokedex from "./pages/pokédex/pokedex.jsx";
 import NotFound from "./pages/notFound/notFound.jsx";
 import NavBar from "./Components/navbar/navBar.jsx";
+import Footer from "./Components/footer/footer.jsx";
 
 function App() {
     // import.meta.env.VITE_API_KEY
@@ -25,7 +24,7 @@ function App() {
             <Route path="/" element={<Pokedex />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
-
+<Footer />
     </>
   )
 }
---
+++ src/Components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css';
+
+function Footer() {
+    return (
+    <>
+        <footer className="standardFooter">
+            <p>&#169; 2026 Pokédex app</p>
+        </footer>
+    </>
+    );
+}
+
+export default Footer;
