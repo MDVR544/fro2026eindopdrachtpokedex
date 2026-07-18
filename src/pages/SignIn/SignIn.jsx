ef4ffd89ca9f3dcc105ba23697a06b7b36bb466c
MDVR544 <devriescool@gmail.com>
2026-07-18 15:23:52 +0200

    Base inlog function and JSON

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,18 +1,75 @@
 import './SignIn.css';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { AuthContext } from '/src/context/AuthContext.jsx';
+import axios from 'axios';
 
 function SignIn(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, toggleError] = useState(false);
+    const { login } = useContext(AuthContext);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        toggleError(false);
+
+        try {
+            const result = await axios.post('http://localhost:3000/login', {
+                email: email,
+                password: password,
+            });
+            // log het resultaat in de console
+            console.log(result.data);
+
+            // geef de JWT token aan de login-functie van de context mee
+            login(result.data.accessToken);
+
+        } catch(e) {
+            console.error(e);
+            toggleError(true);
+        }
+    }
 
     return (
         <>
-            <h1>Sign in Page</h1>
+            <h1>Inloggen</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email-field">
+                    E-mailadres:
+                    <input
+                        type="email"
+                        id="email-field"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+
+                <label htmlFor="password-field">
+                    Wachtwoord:
+                    <input
+                        type="password"
+                        id="password-field"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                {error && <p className="error">Combinatie van e-mailadres en wachtwoord is onjuist</p>}
+
+                <button
+                    type="submit"
+                    className="form-button"
+                >
+                    Inloggen
+                </button>
+            </form>
 
             <p>No account yet? please sign up <Link to="/signup">here</Link>.</p>
-
         </>
-    )
-
+    );
 }
 
 export default SignIn;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,17 +1,87 @@
 import './SignUp.css';
-import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function SignUP(){
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, toggleError] = useState(false);
+    const [loading, toggleLoading] = useState(false);
+    const navigate = useNavigate();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        toggleError(false);
+        toggleLoading(true);
+
+        try {
+            await axios.post('http://localhost:3000/register', {
+                email: email,
+                password: password,
+                username: username,
+            });
+
+            navigate('/signin');
+        } catch(e) {
+            console.error(e);
+            toggleError(true);
+        }
+
+        toggleLoading(false);
+    }
 
     return (
         <>
-            <h1>Sign up Page</h1>
+            <h1>Registreren</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email-field">
+                    E-mailadres:
+                    <input
+                        type="email"
+                        id="email-field"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+
+                <label htmlFor="username-field">
+                    Gebruikersnaam:
+                    <input
+                        type="text"
+                        id="username-field"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </label>
+
+                <label htmlFor="password-field">
+                    Wachtwoord:
+                    <input
+                        type="password"
+                        id="password-field"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                {error && <p className="error">This account already exist, please select a different e-mailadres</p>}
+                <button
+                    type="submit"
+                    className="form-button"
+                    disabled={loading}
+                >
+                    Registreren
+                </button>
+
+            </form>
+
             <p>Already have an account? please sign in <Link to="/signin">here</Link>.</p>
-
         </>
-    )
-
+    );
 }
 
 export default SignUP;
