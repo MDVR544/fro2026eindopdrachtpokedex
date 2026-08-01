212a2eb384b293a9703bbe6a6b302b1c3a64076d
MDVR544 <devriescool@gmail.com>
2026-08-01 16:37:18 +0200

    account styled

--- src/pages/account/account.jsx
+++ src/pages/account/account.jsx
@@ -2,6 +2,7 @@ import './account.css' ;
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '/src/context/AuthContext';
+import pokemonBackground from "../../assets/PokemonBackground.jpg";
 
 
 
@@ -9,15 +10,24 @@ function Account(){
     const { user } = useContext(AuthContext);
 
     return (
-        <div className="account-page">
-            <h1>Profielpagina</h1>
-            <section>
-                <h2>Gegevens</h2>
-                <p><strong>Email:</strong> {user.email}</p>
-            </section>
-
-            <p>Terug naar de <Link to="/">Homepagina</Link></p>
-        </div>
+        <>
+            <img className="background"
+                 src={pokemonBackground}
+                 alt="PokemonBackground"
+            />
+            <div className="account-content-wrapper">
+                <div className="account-content">
+                    <h2>User details</h2>
+                    <section className="user-details" >
+                        <p><strong>Email:</strong> {user.email}</p>
+                    </section>
+                    <div className="navigation-text-content">
+                    <p>Klik <Link to="/myteams">hier</Link> om je eerste team aan te maken!</p>
+                    <p>Terug naar de Pokédex? klik <Link to="/">hier</Link></p>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
